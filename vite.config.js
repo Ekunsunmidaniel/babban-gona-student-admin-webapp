d588da674dcc0cf1260c7ae8445c440e02c483b9
Ekunsunmidaniel <danielekunsunmi@gmail.com>
2023-11-29 09:02:23 +0100

    test build

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,36 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import fs from "fs/promises";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import fs from 'fs/promises'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      src: resolve(__dirname, "src"),
-    },
-  },
-  optimizeDeps: {
-    exclude: ["fsevents"],
-  },
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.jsx?$/,
-    exclude: [],
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        {
-          name: "load-js-files-as-jsx",
-          setup(build) {
-            build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
-              loader: "jsx",
-              contents: await fs.readFile(args.path, "utf8"),
-            }));
-          },
-        },
-      ],
-    },
-  },
-  plugins: [react()],
-});
+	resolve: {
+		alias: {
+			src: resolve(__dirname, 'src'),
+		},
+	},
+	optimizeDeps: {},
+	esbuild: {
+		loader: 'jsx',
+		include: /src\/.*\.jsx?$/,
+		exclude: [],
+	},
+	optimizeDeps: {
+		exclude: ['fsevents'],
+		esbuildOptions: {
+			plugins: [
+				{
+					name: 'load-js-files-as-jsx',
+					setup(build) {
+						build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
+							loader: 'jsx',
+							contents: await fs.readFile(args.path, 'utf8'),
+						}))
+					},
+				},
+			],
+		},
+	},
+	plugins: [react()],
+})
